ab0906383066babb347740cb7ea57a85e471e48e
pmjtoca <pmjtoca@gmail.com>
2014-05-20 22:46:32 +0100

    new title in index

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-05-20T22:27:04+01:00';
+        s.src = '/js/' + language + '/search.js?2014-05-20T22:46:15+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
