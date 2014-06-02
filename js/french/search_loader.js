91591ce0e1daf51cab4330ba11ebfd2e2163d682
pmjtoca <pmjtoca@gmail.com>
2014-06-02 09:05:47 +0100

    new post: resultats des elections europeennes

--- js/french/search_loader.js
+++ js/french/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-06-02T09:04:53+01:00';
+        s.src = '/js/' + language + '/search.js?2014-06-02T09:05:37+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
