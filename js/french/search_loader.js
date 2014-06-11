060278cf00b05acfe5ed8afade986a338f641814
pmjtoca <pmjtoca@gmail.com>
2014-06-11 22:31:17 +0100

    french: Relecture...salaire a vie, in title

--- js/french/search_loader.js
+++ js/french/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-06-11T20:37:51+01:00';
+        s.src = '/js/' + language + '/search.js?2014-06-11T22:31:03+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
