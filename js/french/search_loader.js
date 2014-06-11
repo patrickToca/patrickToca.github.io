73b1dfce4f7f1767c215e38316779c0421853f52
pmjtoca <pmjtoca@gmail.com>
2014-06-11 18:44:42 +0100

    CSS: menu improvement

--- js/french/search_loader.js
+++ js/french/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-06-11T18:25:40+01:00';
+        s.src = '/js/' + language + '/search.js?2014-06-11T18:44:35+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
