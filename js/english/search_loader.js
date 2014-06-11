52458a303dbd312c6d66a0871f2e6e71fb854b90
pmjtoca <pmjtoca@gmail.com>
2014-06-11 08:55:50 +0100

    new Post: 6 June 1944

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-06-02T14:11:05+01:00';
+        s.src = '/js/' + language + '/search.js?2014-06-11T08:55:08+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
