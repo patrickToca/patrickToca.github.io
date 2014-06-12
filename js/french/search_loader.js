b05f5cbb4460f535af40c29facc9a5ba1c03632a
pmjtoca <pmjtoca@gmail.com>
2014-06-12 10:47:38 +0100

    articles: Sane concurrency with Go

--- js/french/search_loader.js
+++ js/french/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-06-11T23:23:53+01:00';
+        s.src = '/js/' + language + '/search.js?2014-06-12T10:46:38+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
