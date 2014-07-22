00f2500682797dda982a799122ad8355444eb9d6
pmjtoca <pmjtoca@gmail.com>
2014-07-22 09:22:01 +0100

    added article link: proof of Pythagoras theorem

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-07-22T09:19:51+01:00';
+        s.src = '/js/' + language + '/search.js?2014-07-22T09:21:53+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
