802ead84ce607c4a7b053b5e252ea9ade6f8aedd
pmjtoca <pmjtoca@gmail.com>
2014-07-23 15:20:22 +0100

    new post: the brink of mass extinction

--- js/french/search_loader.js
+++ js/french/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-07-23T15:07:36+01:00';
+        s.src = '/js/' + language + '/search.js?2014-07-23T15:19:31+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
