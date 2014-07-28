1d45a39dd81c328d964fb3ac2d1117369d9a0eb1
pmjtoca <pmjtoca@gmail.com>
2014-07-28 11:18:08 +0100

    added post: PODEMOS

--- js/english/search_loader.js
+++ js/english/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2014-07-28T11:11:55+01:00';
+        s.src = '/js/' + language + '/search.js?2014-07-28T11:17:58+01:00';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
