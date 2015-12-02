ddb01d2fb5f5852b0545b44bd062a1855900be0f
pmjtoca <pmjtoca@gmail.com>
2015-12-02 23:56:52 +0000

    Added article: une autre histoire de la Securite sociale

--- js/french/search_loader.js
+++ js/french/search_loader.js
@@ -3,7 +3,7 @@
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = '/js/' + language + '/search.js?2015-11-26T10:03:57Z';
+        s.src = '/js/' + language + '/search.js?2015-12-02T23:54:58Z';
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);
     }
